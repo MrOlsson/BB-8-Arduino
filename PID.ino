666a9008a6053b017152c81d91dd606da45e65a2
MrOlsson <anton-olsson@live.com>
2018-05-04 12:51:50 +0200

    I dont even know anymore :'(

--- PID.ino
+++ PID.ino
@@ -8,7 +8,8 @@ DualVNH5019MotorShield md;
 // Right motor == Motor 2
 //#define MotorMaxValue 400
 #define MotorMaxValue 12
-#define BluetoothViewer
+//#define BluetoothViewer
+#define angularBoost 3
 //functions included from motor shield library 
 
 // Define PID regulator parameters and variables
@@ -17,40 +18,44 @@ long cur_t;
 long last_t;
 
 
-#define voltageToMotorspeed 400 / 12              //parameter for att konvertera spänningen till input till motorkontrollern.
-double aggKp=5, aggKi=9.2, aggKd=0;
+#define voltageToMotorspeed 400/12              //parameter for att konvertera spänningen till input till motorkontrollern.
+double aggKp=3.6994, aggKi = 14.57, aggKd=-0.711;
 double consKp=2, consKi=4.2, consKd=0;
 
-double L_Input, L_Output, L_Setpoint, L_Gap, R_Input, R_Output, R_Setpoint, R_Gap;
+double L_Input, L_Output = 0, L_Setpoint, L_Gap, R_Input, R_Output = 0, R_Setpoint, R_Gap;
+
 
 double omega_till_hast = 0.25*0.27/(2*0.045*4); // R*L/(2*r*N)
 #define wheelrotationToSphereSpeed 0.05 
 
 #define MaximumVelocity 0.25
-#define MaximumOmega 0.75
+
 
 //Specify the links and initial tuning parameters
 PID L_PID(&L_Input, &L_Output, &L_Setpoint, consKp, consKi, consKd, DIRECT);
 PID R_PID(&R_Input, &R_Output , &R_Setpoint, consKp, consKi, consKd, DIRECT);
+
 
 void PIDSetup(){
   md.init();
   //turn the PID on
   L_PID.SetMode(AUTOMATIC);
   R_PID.SetMode(AUTOMATIC);
+
   /*L_PID.SetSampleTime(1000);
   R_PID.SetSampleTime(1000);*/
   L_PID.SetOutputLimits(-MotorMaxValue, MotorMaxValue);
   R_PID.SetOutputLimits(-MotorMaxValue, MotorMaxValue);
-
+  
   L_PID.SetSampleTime(PIDSampleTime);
   R_PID.SetSampleTime(PIDSampleTime);
+
 }
 
 void PIDLoop(){
   
-  L_Setpoint = (MaximumVelocity * input.velocity + omega_till_hast * input.omega * MaximumOmega);
-  R_Setpoint = (MaximumVelocity * input.velocity - omega_till_hast * input.omega * MaximumOmega);
+  L_Setpoint = (MaximumVelocity * input.velocity);
+  R_Setpoint = (MaximumVelocity * input.velocity);
   
   L_Input = getRads().L_wheel * wheelrotationToSphereSpeed;
   R_Input = getRads().R_wheel * wheelrotationToSphereSpeed;
@@ -60,7 +65,7 @@ void PIDLoop(){
   L_Gap = L_Setpoint - L_Input; // distance away from setpoint
   R_Gap = R_Setpoint - R_Input;
 
-  if(abs(L_Gap) < 0.05 || (!L_Setpoint && !R_Setpoint))
+  if(abs(L_Gap) > 0.02 && L_Setpoint)
   {  //we're close to setpoint, use conservative tuning parameters
     L_PID.SetTunings(consKp, consKi, consKd);
   }
@@ -69,7 +74,7 @@ void PIDLoop(){
      L_PID.SetTunings(aggKp, aggKi, aggKd);
   }
 
-  if(abs(R_Gap) < 0.05) // Byt ut 1 < 10 till gap < 10
+  if(abs(R_Gap) < 0.02 && L_Setpoint) // Byt ut 1 < 10 till gap < 10
   {  //we're close to setpoint, use conservative tuning parameters
     R_PID.SetTunings(consKp, consKi, consKd);
   }
@@ -80,13 +85,8 @@ void PIDLoop(){
 
   L_PID.Compute();
   R_PID.Compute();
-
-  //setMotorSpeed(L_Output, R_Output);
-  if(L_Setpoint == 0 && R_Setpoint == 0 && abs(L_Output) < 1.5 && abs(R_Output) < 1.5){
-      L_Output = 0;
-      R_Output = 0; 
-  }
-  setMotorSpeed(L_Output * voltageToMotorspeed, R_Output * voltageToMotorspeed);
+  
+  setMotorSpeed(L_Output * voltageToMotorspeed + input.omega*angularBoost * voltageToMotorspeed, R_Output * voltageToMotorspeed - angularBoost * input.omega * voltageToMotorspeed);
 }
 
 void stopIfFault()
@@ -104,10 +104,20 @@ void stopIfFault()
 }
 
 void setMotorSpeed(int LeftSpeed, int RightSpeed){ //set speed > 400 to keep old motor speed
-  if(LeftSpeed > -400 && LeftSpeed < 400)
+  if(LeftSpeed < -400){
+    md.setM1Speed(-400);
+  }else if(LeftSpeed > 400){
+    md.setM1Speed(400);
+  }else{
     md.setM1Speed(LeftSpeed);
-  if(RightSpeed > -400 && RightSpeed < 400)
+  }
+  if(RightSpeed < -400){
+    md.setM2Speed(-400);
+  }else if(RightSpeed > 400){
+    md.setM2Speed(400);
+  }else{
     md.setM2Speed(RightSpeed);
+  }
 }
 
 void printPidIO(bool newline){
